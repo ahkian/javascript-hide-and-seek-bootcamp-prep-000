ad75a82ebe942fd5cf589db7018d74d34b3320af
Daniel Brazel <dbrazel4@gmail.com>
2018-01-26 22:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n){
 
 function deepestChild(){
   var currElement = document.getElementById('grand-node')
-  while (tempElement.querySelector('div') !== null){
+  while (currElement.querySelector('div') !== null){
     currElement = currElement.querySelector('div')
   }
   return currElement
