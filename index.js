01528cd62cea609f1fd93ad6b438c5ecc864cb6d
Daniel Brazel <dbrazel4@gmail.com>
2018-01-26 22:40:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  var el = document.getElementById('nested')
+  return el.querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var node = document.querySelectorAll('ul.ranked-list li')
+  for (let x = 0; x < node.length; x++){
+    var inner = node[x].innerHTML
+    node[x].innerHTML = (parseInt(inner) + n).toString()
+  }
+}
+
+function deepestChild(){
+  var currElement = document.getElementById('grand-node')
+  while (currElement.querySelector('div') !== null){
+    currElement = currElement.querySelector('div')
+  }
+  return currElement
+}
